7435443038573f9871043f2deba1348b69bec1fc
Emy1pa <souaouti.iman@gmail.com>
2025-06-08 22:02:14 +0100

    Solve Problem 10: Read three marks and calculate their average

--- 50 Problem-Solving Tasks/50 Problem-Solving Tasks.cpp
+++ 50 Problem-Solving Tasks/50 Problem-Solving Tasks.cpp
@@ -1,27 +1,30 @@
 #include <iostream>
 using namespace std;
 
-void ReadNumbers(int &Num1, int &Num2, int &Num3){
-    cout << "Please enter the first Number ? \n";
-    cin >> Num1;
-    cout << "Please enter the second Number ? \n";
-    cin >> Num2;
-    cout << "Please enter the third Number ? \n";
-    cin >> Num3;
+void ReadMarks(int &Mark1, int &Mark2, int &Mark3){
+    cout << "Please enter the first Mark ? \n";
+    cin >> Mark1;
+    cout << "Please enter the second Mark ? \n";
+    cin >> Mark2;
+    cout << "Please enter the third Mark ? \n";
+    cin >> Mark3;
 }
 
-int CalculateSum(int Num1, int Num2, int Num3){
-    return Num1 + Num2 + Num3;
+int CalculateSumOf3Marks(int Mark1, int Mark2, int Mark3){
+    return Mark1 + Mark2 + Mark3;
 }
-void PrintSum(int Sum){
-    cout << "Sum is: " << Sum << endl;
+float AverageOf3Marks(int Mark1, int Mark2, int Mark3){
+    return (float)CalculateSumOf3Marks(Mark1, Mark2, Mark3) / 3;
+}
+void PrintAverage(float Average){
+    cout << "Average is: " << Average << endl;
 }
 
 int main() {
 
-    int Num1, Num2, Num3;
-    ReadNumbers(Num1, Num2, Num3);
-    PrintSum(CalculateSum(Num1, Num2, Num3));
+    int Mark1, Mark2, Mark3;
+    ReadMarks(Mark1, Mark2, Mark3);
+    PrintAverage(AverageOf3Marks(Mark1, Mark2, Mark3));
     return 0;
 }
 

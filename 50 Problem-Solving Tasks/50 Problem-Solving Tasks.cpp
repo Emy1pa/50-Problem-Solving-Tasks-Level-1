869e96b12c541c49ce45e5459b03f72303bd55df
Emy1pa <souaouti.iman@gmail.com>
2025-06-08 22:17:42 +0100

    Solve Problem 12: Read two numbers and print the maximum

--- 50 Problem-Solving Tasks/50 Problem-Solving Tasks.cpp
+++ 50 Problem-Solving Tasks/50 Problem-Solving Tasks.cpp
@@ -1,36 +1,28 @@
 #include <iostream>
 using namespace std;
 
-
-
-void ReadMarks(int &Mark1, int &Mark2, int &Mark3){
-    cout << "Please enter the first Mark ? \n";
-    cin >> Mark1;
-    cout << "Please enter the second Mark ? \n";
-    cin >> Mark2;
-    cout << "Please enter the third Mark ? \n";
-    cin >> Mark3;
+void ReadNumbers(int &Num1, int &Num2){
+    cout << "Please enter Number 1 ? \n";
+    cin >> Num1;
+    cout << "Please enter Number 2 ? \n";
+    cin >> Num2;
 }
 
-int CalculateSumOf3Marks(int Mark1, int Mark2, int Mark3){
-    return Mark1 + Mark2 + Mark3;
-}
-float AverageOf3Marks(int Mark1, int Mark2, int Mark3){
-    return (float)CalculateSumOf3Marks(Mark1, Mark2, Mark3) / 3;
-}
-void PrintAverage(float Average){
-    cout << "Average is: " << Average << endl;
-    if (Average >= 50)
-        cout << "Pass\n";
+int CheckMaxNumber(int Num1, int Num2){
+    if (Num1 > Num2)
+        return Num1;
     else
-        cout << "Fail\n";
+        return Num2;
 }
+void PrintMaxNumber(int Max){
+    cout << "Max Number is: " << Max << endl;
+}
+
 
 int main() {
-
-    int Mark1, Mark2, Mark3;
-    ReadMarks(Mark1, Mark2, Mark3);
-    PrintAverage(AverageOf3Marks(Mark1, Mark2, Mark3));
+    int Num1, Num2;
+    ReadNumbers(Num1, Num2);
+    PrintMaxNumber(CheckMaxNumber(Num1, Num2));
     return 0;
 }
 

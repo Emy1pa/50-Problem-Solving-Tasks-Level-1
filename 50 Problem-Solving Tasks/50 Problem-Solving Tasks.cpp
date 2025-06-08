1188f679b21bc55139b58917f7a49860493eb774
Emy1pa <souaouti.iman@gmail.com>
2025-06-08 22:23:53 +0100

    Solve Problem 13: Read three numbers and print the maximum

--- 50 Problem-Solving Tasks/50 Problem-Solving Tasks.cpp
+++ 50 Problem-Solving Tasks/50 Problem-Solving Tasks.cpp
@@ -1,18 +1,26 @@
 #include <iostream>
 using namespace std;
 
-void ReadNumbers(int &Num1, int &Num2){
+void ReadNumbers(int &Num1, int &Num2, int &Num3){
     cout << "Please enter Number 1 ? \n";
     cin >> Num1;
     cout << "Please enter Number 2 ? \n";
     cin >> Num2;
+    cout << "Please enter Number 3 ? \n";
+    cin >> Num3;
 }
 
-int CheckMaxNumber(int Num1, int Num2){
+int CheckMaxNumber(int Num1, int Num2, int Num3){
     if (Num1 > Num2)
-        return Num1;
+        if (Num1 > Num3)
+            return Num1;
+        else
+            return Num3;
     else
-        return Num2;
+        if (Num2 > Num3)
+            return Num2;
+        else
+            return Num3;
 }
 void PrintMaxNumber(int Max){
     cout << "Max Number is: " << Max << endl;
@@ -20,9 +28,9 @@ void PrintMaxNumber(int Max){
 
 
 int main() {
-    int Num1, Num2;
-    ReadNumbers(Num1, Num2);
-    PrintMaxNumber(CheckMaxNumber(Num1, Num2));
+    int Num1, Num2, Num3;
+    ReadNumbers(Num1, Num2, Num3);
+    PrintMaxNumber(CheckMaxNumber(Num1, Num2, Num3));
     return 0;
 }
 

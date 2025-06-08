7f909c4e5bf187ff624c8b64771f30a970e84483
Emy1pa <souaouti.iman@gmail.com>
2025-06-08 15:25:10 +0100

    Solve Problem 2: Add ReadName function and display entered name

--- 50 Problem-Solving Tasks/50 Problem-Solving Tasks.cpp
+++ 50 Problem-Solving Tasks/50 Problem-Solving Tasks.cpp
@@ -1,12 +1,20 @@
 #include <iostream>
 using namespace std;
 
+string ReadName() {
+    string Name;
+    cout << "Please enter your name ? \n";
+    cin >> Name;
+    return Name;
+}
+
 void DisplayName(string Name){
+
     cout << "\n Your Name is: " << Name << endl;
 }
 
 int main()
 {
-    DisplayName("Imane");
+    DisplayName(ReadName());
 }
 

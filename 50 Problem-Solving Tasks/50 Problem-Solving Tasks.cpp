025a6731e9066a763e420aaf3399b87a924fcddd
Emy1pa <souaouti.iman@gmail.com>
2025-06-10 15:47:24 +0100

    Solve Problem 22: Calculate circle area using triangle sides A and B

--- 50 Problem-Solving Tasks/50 Problem-Solving Tasks.cpp
+++ 50 Problem-Solving Tasks/50 Problem-Solving Tasks.cpp
@@ -2,27 +2,29 @@
 #include <cmath>
 using namespace std;
 
-float ReadCircumference(){
-    float L;
-    cout << "Please enter L Circumference value ? \n";
-    cin >> L;
-    return L;
+void ReadTriangleData(float &A, float &B){
+    
+    cout << "Please enter Triangle Side A ? \n";
+    cin >> A;
+    cout << "Please enter Triangle Side B ? \n";
+    cin >> B;
     
 }
 
-float CalculateCircleAreaByCircumference(float L) {
+float CalculateCircleAreaByTriangle(float A, float B) {
     const float PI = 3.141592653589793238;
-    float Area = pow(L, 2) / (4 * PI);
+    float Area = (PI * pow(B,2) /4) * ((2*A-B) /(2*A+B));
     return Area;
 }
 void PrintResult(float Area){
-    cout << "Circle Area By Circumference is: " << Area << endl;
+    cout << "Circle Area By Triangle is: " << Area << endl;
 }
 
 int main() {
    
-    float D;
-    PrintResult(CalculateCircleAreaByCircumference(ReadCircumference()));
+    float A, B;
+    ReadTriangleData(A, B);
+    PrintResult(CalculateCircleAreaByTriangle(A, B));
     return 0;
 }
 

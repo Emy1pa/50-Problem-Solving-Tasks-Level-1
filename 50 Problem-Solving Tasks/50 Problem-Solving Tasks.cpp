98c86753e4de1f1a5b43f6ae3dfa555204d34667
Emy1pa <souaouti.iman@gmail.com>
2025-06-10 16:22:48 +0100

    Solve Problem 24: Age Validation: Modular program to check if age is between 18 and 45

--- 50 Problem-Solving Tasks/50 Problem-Solving Tasks.cpp
+++ 50 Problem-Solving Tasks/50 Problem-Solving Tasks.cpp
@@ -1,35 +1,25 @@
 #include <iostream>
-#include <cmath>
 using namespace std;
 
-void ReadTriangleData(float &A, float &B, float &C){
-    
-    cout << "Please enter Triangle Side A ? \n";
-    cin >> A;
-    cout << "Please enter Triangle Side B ? \n";
-    cin >> B;
-    cout << "Please enter Triangle Side C ? \n";
-    cin >> C;
-    
+int ReadAge() {
+    int Age;
+    cout << "Please enter your age ? \n";
+    cin >> Age;
+    return Age;
 }
-
-float CalculateCircleAreaByTriangle(float A, float B, float C) {
-    const float PI = 3.141592653589793238;
-    float p = (A + B + C) / 2;
-    float T = (A * B * C) / (4 * sqrt(p * (p - A) * (p - B) * (p - C)));
-
-    float Area = PI * pow(T, 2);
-    return Area;
+bool ValidateNumberInRange(int Age, int From, int To){
+    return Age >= From && Age <= To;
 }
-void PrintResult(float Area){
-    cout << "Circle Area By Triangle is: " << Area << endl;
+void PrintResult(int Age){
+    if (ValidateNumberInRange(Age, 18, 45))
+        cout << "Valid Age \n";
+    else
+        cout << "Invalid Age \n";
 }
 
 int main() {
    
-    float A, B, C;
-    ReadTriangleData(A, B, C);
-    PrintResult(CalculateCircleAreaByTriangle(A, B, C));
+    PrintResult(ReadAge());
     return 0;
 }
 

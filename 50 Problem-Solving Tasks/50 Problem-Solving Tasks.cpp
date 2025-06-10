44c00b8b1310932b9a908de4e28cdcb6292a5877
Emy1pa <souaouti.iman@gmail.com>
2025-06-10 12:59:56 +0100

    Solve Problem 20: Calculate circle area inscribed in a square

--- 50 Problem-Solving Tasks/50 Problem-Solving Tasks.cpp
+++ 50 Problem-Solving Tasks/50 Problem-Solving Tasks.cpp
@@ -2,27 +2,27 @@
 #include <cmath>
 using namespace std;
 
-float ReadDiameter(){
-    float D;
-    cout << "Please enter D value ? \n";
-    cin >> D;
-    return D;
+float ReadSquareSide(){
+    float A;
+    cout << "Please enter A value ? \n";
+    cin >> A;
+    return A;
     
 }
 
-float CalculateCircleAreaThroughDiameter(float D) {
+float CalculateCircleAreaInscribedInSquare(float A) {
     const float PI = 3.141592653589793238;
-    float Area = (PI * pow(D, 2))/4;
+    float Area = (PI * pow(A, 2))/4;
     return Area;
 }
-void PrintCircleThroughDiameter(float Area){
-    cout << "Circle Area Through Diameter is: " << Area << endl;
+void PrintCircleInscribedInSquare(float Area){
+    cout << "Circle Area Inscribed in a Square is: " << Area << endl;
 }
 
 int main() {
    
     float D;
-    PrintCircleThroughDiameter(CalculateCircleAreaThroughDiameter(ReadDiameter()));
+    PrintCircleInscribedInSquare(CalculateCircleAreaInscribedInSquare(ReadSquareSide()));
     return 0;
 }
 

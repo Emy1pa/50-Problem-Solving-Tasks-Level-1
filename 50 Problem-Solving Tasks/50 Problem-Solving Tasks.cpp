610eecb54aa437ea0b984f805350887692ece007
Emy1pa <souaouti.iman@gmail.com>
2025-06-10 12:52:51 +0100

    Solve Problem 19: Calculate circle area using through diameter

--- 50 Problem-Solving Tasks/50 Problem-Solving Tasks.cpp
+++ 50 Problem-Solving Tasks/50 Problem-Solving Tasks.cpp
@@ -2,26 +2,27 @@
 #include <cmath>
 using namespace std;
 
-void ReadRadious(float &R){
-    cout << "Please enter R value ? \n";
-    cin >> R;
+float ReadDiameter(){
+    float D;
+    cout << "Please enter D value ? \n";
+    cin >> D;
+    return D;
     
 }
 
-float CalculateCircleArea(float R) {
-    const float PI = 3.14;
-    float Area = PI * pow(R, 2);
+float CalculateCircleAreaThroughDiameter(float D) {
+    const float PI = 3.141592653589793238;
+    float Area = (PI * pow(D, 2))/4;
     return Area;
 }
-void PrintCircle(float Area){
-    cout << "Circle Area is: " << Area << endl;
+void PrintCircleThroughDiameter(float Area){
+    cout << "Circle Area Through Diameter is: " << Area << endl;
 }
 
 int main() {
    
-    float R;
-    ReadRadious(R);
-    PrintCircle(CalculateCircleArea(R));
+    float D;
+    PrintCircleThroughDiameter(CalculateCircleAreaThroughDiameter(ReadDiameter()));
     return 0;
 }
 

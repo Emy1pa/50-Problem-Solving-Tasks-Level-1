871edd83fd63c698745de3a4199a599595065b86
Emy1pa <souaouti.iman@gmail.com>
2025-06-10 09:28:21 +0100

    Solve Problem 14: Read two numbers, swap them and display before/after values

--- 50 Problem-Solving Tasks/50 Problem-Solving Tasks.cpp
+++ 50 Problem-Solving Tasks/50 Problem-Solving Tasks.cpp
@@ -1,36 +1,30 @@
 #include <iostream>
 using namespace std;
 
-void ReadNumbers(int &Num1, int &Num2, int &Num3){
+void ReadNumbers(int &Num1, int &Num2){
     cout << "Please enter Number 1 ? \n";
     cin >> Num1;
     cout << "Please enter Number 2 ? \n";
     cin >> Num2;
-    cout << "Please enter Number 3 ? \n";
-    cin >> Num3;
 }
 
-int CheckMaxNumber(int Num1, int Num2, int Num3){
-    if (Num1 > Num2)
-        if (Num1 > Num3)
-            return Num1;
-        else
-            return Num3;
-    else
-        if (Num2 > Num3)
-            return Num2;
-        else
-            return Num3;
+void SwapNumber(int &Num1, int &Num2){
+    int Temp = Num1;
+    Num1 = Num2;
+    Num2 = Temp;
 }
-void PrintMaxNumber(int Max){
-    cout << "Max Number is: " << Max << endl;
-}
+
+
 
 
 int main() {
-    int Num1, Num2, Num3;
-    ReadNumbers(Num1, Num2, Num3);
-    PrintMaxNumber(CheckMaxNumber(Num1, Num2, Num3));
+    int Num1, Num2;
+    ReadNumbers(Num1, Num2);
+    cout << "Num1 & Num2 Before SWAP\n";
+    cout << "Num1 = " << Num1 << " Num2 = " << Num2 << endl;
+    SwapNumber(Num1, Num2);
+    cout << "Num1 & Num2 After SWAP\n";
+    cout << "Num1 = " << Num1 << " Num2 = " << Num2 << endl;
     return 0;
 }
 

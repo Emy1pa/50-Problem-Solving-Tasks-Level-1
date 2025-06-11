ee3d967dabd48dd4bb92880641ceb6705f0c9015
Emy1pa <souaouti.iman@gmail.com>
2025-06-11 11:08:53 +0100

    Solve Problem 28: Calculate the sum of odd numbers from 1 to N

--- 50 Problem-Solving Tasks/50 Problem-Solving Tasks.cpp
+++ 50 Problem-Solving Tasks/50 Problem-Solving Tasks.cpp
@@ -1,44 +1,37 @@
 #include <iostream>
 using namespace std;
 
+enum enOddEven {Odd = 1, Even = 2};
+
 int ReadNumber() {
     int Number;
     cout << "Please enter a number ? \n";
     cin >> Number;
     return Number;
 }
-void PrintNumbersFromNTo1UsingForLoop(int Number) {
-    cout << "Print Numbers using For Loop \n";
-    for (int i = Number; i >= 1; i--)
+enOddEven CheckEvenOrOdd(int Number) {
+    if (Number % 2 != 0)
+        return enOddEven::Odd;
+    else
+        return enOddEven::Even;
+}
+int PrintSumOfOddNumbers(int Number){
+    int sum = 0;
+    for (int counter = 1; counter <= Number; counter++)
     {
-        cout << i << endl;
+        if (CheckEvenOrOdd(counter) == enOddEven::Odd)
+            sum += counter;
     }
+    return sum;
 }
-void PrintNumbersFromNto1UsingWhileLoop(int Number) {
-    cout << "Print Numbers using While Loop \n";
-    int i = Number;
-    while (i >= 1) {
-        cout << i << endl;
-        i--;
-    }
-}
-void PrintNumbersFromNto1UsingDoWhileLoop(int Number) {
-    cout << "Print Numbers using Do While Loop \n";
-    int i = Number;
-    do
-    {
-        cout << i << endl;
-        i--;
-    } while (i>=1);
-}
+
+
 
 
 int main() {
    
     int Number = ReadNumber();
-    PrintNumbersFromNTo1UsingForLoop(Number);
-    PrintNumbersFromNto1UsingWhileLoop(Number);
-    PrintNumbersFromNto1UsingDoWhileLoop(Number);
+    cout << "Sum = " << PrintSumOfOddNumbers(Number) << endl;
     return 0;
 }
 

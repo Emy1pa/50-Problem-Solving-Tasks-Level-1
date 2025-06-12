3f58223be7607719731a4b98c7a034c024ace37e
Emy1pa <souaouti.iman@gmail.com>
2025-06-12 12:08:52 +0100

    Solve Problem 38: Determine if a number is prime or not

--- 50 Problem-Solving Tasks/50 Problem-Solving Tasks.cpp
+++ 50 Problem-Solving Tasks/50 Problem-Solving Tasks.cpp
@@ -1,32 +1,41 @@
 #include <iostream>
+#include <string>
+#include <cmath>
 using namespace std;
 
-float ReadNumber(string Message){
-    float Number = 0;
-    cout << Message << endl;
-    cin >> Number;
-    return Number;
-}
+enum enPrimeNotPrimeNumber { Prime = 1, NotPrime = 2 };
 
-int CalculateSumUntilMinus99(){
-    float Number;
-    int sum = 0;
+int ReadPositiveNumber(string Message) {
+    int Number;
     do
     {
-        Number = ReadNumber("Please enter a number ?");
-        if (Number != -99) {
-            sum += Number;
-        }
-    } while (Number != -99);
-    return sum;
+     cout << Message << endl;
+     cin >> Number;
+    } while (Number < 0);
+    return Number;
+}
+enPrimeNotPrimeNumber CheckPrimeNumber(int Number){
+    int M = round(Number / 2);
+    for (int i = 2; i <= M; i++)
+    {
+        if (Number % i == 0)
+            return enPrimeNotPrimeNumber::NotPrime;
+        else
+            return enPrimeNotPrimeNumber::Prime;
+    }
 }
 
-
+void PrintResult(int Number) {
+    if (CheckPrimeNumber(Number) == enPrimeNotPrimeNumber::Prime)
+        cout << Number << " is a prime Number \n";
+    else
+        cout << Number << " is not a prime Number \n";
+}
 
 
 int main() {
    
-    cout << CalculateSumUntilMinus99();
+    PrintResult(ReadPositiveNumber("Please enter a positive number ?"));
     return 0;
 }
 

7fad59540fa21992315419502b27aaeeddb976f7
Emy1pa <souaouti.iman@gmail.com>
2025-06-12 11:42:17 +0100

    Solve Problem 37: Calculate the sum of numbers entered until -99 is input

--- 50 Problem-Solving Tasks/50 Problem-Solving Tasks.cpp
+++ 50 Problem-Solving Tasks/50 Problem-Solving Tasks.cpp
@@ -1,41 +1,32 @@
 #include <iostream>
 using namespace std;
 
-void ReadData(int &Num1, int &Num2, char &OpType) {
-    cout << "Please enter the first number ? \n";
-    cin >> Num1;
-    cout << "Please enter the second number ? \n";
-    cin >> Num2;
-    cout << "Please enter the operation Type ? \n";
-    cin >> OpType;
+float ReadNumber(string Message){
+    float Number = 0;
+    cout << Message << endl;
+    cin >> Number;
+    return Number;
 }
-void PrintCalculatedData(int Num1, int Num2, char OpType){
-    switch (OpType)
+
+int CalculateSumUntilMinus99(){
+    float Number;
+    int sum = 0;
+    do
     {
-    case '+':
-        cout <<  Num1 + Num2;
-        break;
-    case '-':
-        cout <<  Num1 - Num2;
-        break;
-    case '*':
-        cout << Num1 * Num2;
-        break;
-    case '/':
-        cout << Num1 / Num2;
-        break;
-    default:
-        cout << "Wrong Operation";
-        break;
-    }
+        Number = ReadNumber("Please enter a number ?");
+        if (Number != -99) {
+            sum += Number;
+        }
+    } while (Number != -99);
+    return sum;
 }
+
+
+
 
 int main() {
    
-    int Num1, Num2;
-    char OpType;
-    ReadData(Num1, Num2, OpType);
-    PrintCalculatedData(Num1, Num2, OpType);
+    cout << CalculateSumUntilMinus99();
     return 0;
 }
 

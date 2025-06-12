cabcdda582ca0ead3d80d596ddda1a1d06f0196d
Emy1pa <souaouti.iman@gmail.com>
2025-06-12 09:50:27 +0100

    Solve Problem 36: Perform basic arithmetic operations based on user input

--- 50 Problem-Solving Tasks/50 Problem-Solving Tasks.cpp
+++ 50 Problem-Solving Tasks/50 Problem-Solving Tasks.cpp
@@ -1,38 +1,41 @@
 #include <iostream>
 using namespace std;
 
-struct stPiggyBankCalculator {
-    int Pennies, Nickles, Dimes, Quarters, Dollars;
-};
-
-stPiggyBankCalculator ReadPiggyBankInfo() {
-    stPiggyBankCalculator PiggyBankInfo;
-    cout << "Please enter the amount of Pennies ? \n";
-    cin >> PiggyBankInfo.Pennies;
-    cout << "Please enter the amount of Nickles ? \n";
-    cin >> PiggyBankInfo.Nickles;
-    cout << "Please enter the amount of Dimes ? \n";
-    cin >> PiggyBankInfo.Dimes;
-    cout << "Please enter the amount of Quarters ? \n";
-    cin >> PiggyBankInfo.Quarters;
-    cout << "Please enter the amount of Dollars ? \n";
-    cin >> PiggyBankInfo.Dollars;
-    return PiggyBankInfo;
+void ReadData(int &Num1, int &Num2, char &OpType) {
+    cout << "Please enter the first number ? \n";
+    cin >> Num1;
+    cout << "Please enter the second number ? \n";
+    cin >> Num2;
+    cout << "Please enter the operation Type ? \n";
+    cin >> OpType;
 }
-
-int CalculateTotalPennies(stPiggyBankCalculator PiggyBankInfo){
-    int TotalPennies =  PiggyBankInfo.Pennies + PiggyBankInfo.Nickles * 5 +
-        PiggyBankInfo.Dimes * 10 + PiggyBankInfo.Quarters * 25 + PiggyBankInfo.Dollars * 100;
-    return TotalPennies;
+void PrintCalculatedData(int Num1, int Num2, char OpType){
+    switch (OpType)
+    {
+    case '+':
+        cout <<  Num1 + Num2;
+        break;
+    case '-':
+        cout <<  Num1 - Num2;
+        break;
+    case '*':
+        cout << Num1 * Num2;
+        break;
+    case '/':
+        cout << Num1 / Num2;
+        break;
+    default:
+        cout << "Wrong Operation";
+        break;
+    }
 }
-
 
 int main() {
    
-    stPiggyBankCalculator PiggyBankInfo;
-    float TotalPennies = CalculateTotalPennies(ReadPiggyBankInfo());
-    cout << "Total Pennies = " << TotalPennies << endl;
-    cout << "Amount of Dollars = " << TotalPennies / 100 << endl;
+    int Num1, Num2;
+    char OpType;
+    ReadData(Num1, Num2, OpType);
+    PrintCalculatedData(Num1, Num2, OpType);
     return 0;
 }
 

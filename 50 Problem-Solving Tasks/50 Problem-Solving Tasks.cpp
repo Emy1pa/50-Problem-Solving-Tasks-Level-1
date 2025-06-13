8ef150c185cd808cc17d6b8bc7ef646d58a3edc7
Emy1pa <souaouti.iman@gmail.com>
2025-06-13 09:26:17 +0100

    Solve Problem 44: Display day of the week from number using enum

--- 50 Problem-Solving Tasks/50 Problem-Solving Tasks.cpp
+++ 50 Problem-Solving Tasks/50 Problem-Solving Tasks.cpp
@@ -3,6 +3,7 @@
 #include <cmath>
 using namespace std;
 
+enum enDayOfTheWeek {Monday = 1, Tuesday = 2, Wednesday = 3, Thursday = 4, Friday = 5, Saturday = 6, Sunday = 7};
 
 int ReadPositiveNumber(string Message){
     int Number = 0;
@@ -14,29 +15,35 @@ int ReadPositiveNumber(string Message){
 	return Number;
 }
 
-void CalculateNumberOfDays_Minutes_Seconds(int NumberOfSeconds){
-	int SecondsPerDay = 24 * 60 * 60;
-	int HoursPerDay = 60 * 60;
-	int MinutesPerDay =  60;
-
-	int NumberOfDays = NumberOfSeconds / SecondsPerDay;
-	int Remainder = floor(NumberOfSeconds % SecondsPerDay);
-	int NumberOfHours = Remainder / HoursPerDay;
-	Remainder = floor(Remainder % HoursPerDay);
-	int NumberOfMinutes = Remainder / MinutesPerDay;
-	Remainder = floor(Remainder % MinutesPerDay);
-	int SecondsRemainder = Remainder;
-
-	cout << NumberOfDays << ":" << NumberOfHours << ":" << NumberOfMinutes << ":" << SecondsRemainder << endl;
-
+enDayOfTheWeek ReadDayOfTheWeek() {
+	int Day = ReadPositiveNumber("Please choose a specefic day ? (1 - 7)");
+	return (enDayOfTheWeek)Day;
+}
+string DisplayDayOfTheWeek(enDayOfTheWeek DayOfTheWeek){
+	switch (DayOfTheWeek)
+	{
+	case enDayOfTheWeek::Monday:
+		return "It is Monday";
+	case enDayOfTheWeek::Tuesday:
+		return "It is Tuesday";
+	case enDayOfTheWeek::Wednesday:
+		return "It is Wednesday";
+	case enDayOfTheWeek::Thursday:
+		return "It is Thursday";
+	case enDayOfTheWeek::Friday:
+		return "It is Friday";
+	case enDayOfTheWeek::Saturday:
+		return "It is Saturday";
+	case enDayOfTheWeek::Sunday:
+		return "It is Sunday";
+	default:
+		return "Not a Week Day";
+	}
 }
 
-
-
 int main() {
-   
-	int NumberOfSeconds = ReadPositiveNumber("Please enter the number of seconds ? \n");
-	CalculateNumberOfDays_Minutes_Seconds(NumberOfSeconds);
-    return 0;
+    
+	cout << DisplayDayOfTheWeek(ReadDayOfTheWeek()) << endl;
+	return 0;
 }
 
